609bfdaf288ffe450d3da72e212edf1fe572f65b
TechWizard-SS <samir898877.s@gmail.com>
2025-11-14 12:18:44 +0400

    Some commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,85 @@
+plugins {
+    application
+    checkstyle
+    id("org.sonarqube") version "7.0.1.6134"
+    jacoco
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    sourceCompatibility = JavaVersion.VERSION_21
+    targetCompatibility = JavaVersion.VERSION_21
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    violationRules {
+        rule {
+            limit {
+                minimum = "0.5".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
+tasks.checkstyleMain {
+    dependsOn(tasks.compileJava)
+}
+
+tasks.checkstyleTest {
+    dependsOn(tasks.compileTestJava)
+}
+
+checkstyle {
+    toolVersion = "10.12.1"
+    configFile = file("Config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+    isShowViolations = true
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "TechWizard-SS_java-project-72")
+        property("sonar.organization", "techwizard-ss")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.sources", "src/main")
+        property("sonar.tests", "src/test")
+        property("sonar.java.binaries", "build/classes/java/main")
+        property("sonar.java.test.binaries", "build/classes/java/test")
+        property("sonar.java.test.reportPaths", "build/test-results/test")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+        property("sonar.exclusions", "**/build/**,**/generated/**")
+        property("sonar.verbose", "true")
+    }
+}
